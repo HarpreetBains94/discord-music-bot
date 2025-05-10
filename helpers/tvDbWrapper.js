c6d788292ea3edc5162c4514efc16eca8f9846f7
HarpreetBains94 <hazbains20@gmail.com>
2025-05-10 13:20:00 +0100

    No but actually this time

--- helpers/tvDbWrapper.js
+++ helpers/tvDbWrapper.js
@@ -14,15 +14,18 @@ module.exports = class TvDbWrapper {
     return axios.post(URL_BASE + '/login', {
         apikey: TV_DB_API_KEY
       }
-    )
+    ).catch((err) => {
+      console.log('failed to get TVDB token', err);
+      throw new Error('Failed to renew TVDB token');
+    })
   }
 
   async getEmbed(query) {
     return this.getSynopsisData(query).then((data) => {
       const embed = new EmbedBuilder()
-        .setTitle(data.tvDb.extended_title)
+        .setTitle(data.tvDbData.extended_title)
         .setAuthor({ name: 'Data sourced from TheTVTB', url: 'https://www.thetvdb.com/' })
-        .setDescription(`Genres: ${data.tvDb.genres.join(', ')}\n\n${data.tvDb.overviews['eng'] || ''}`);
+        .setDescription(`Genres: ${data.tvDbData.genres.join(', ')}\n\n${data.tvDbData.overviews['eng'] || ''}`);
         return {
           embed,
           trailer: data.trailer,
@@ -32,43 +35,37 @@ module.exports = class TvDbWrapper {
     });
   }
 
-  getSynopsisData(query) {
-    return this.login().then((response) => {
-      return axios.get(`${URL_BASE}/search?query=${encodeURIComponent(query)}`, {
-        headers: `Authorization: Bearer ${response.data.data.token}`
-      }).then((response) => {
-        if (response.status !== 200) {
-          throw new Error('TVDB search request failed');
-        }
-        if (response.data.data.length === 0) {
-          throw new Error('TVDB search returned no results');
-        }
-        const tvDbData = response.data.data[0];
-
-        return youtubesearchapi.GetListByKeyword(query + ' trailer', false, 1, {})
-          .then((res) => {
-            var trailer = '';
-            if (res && res.items && res.items.length !== 0) {
-              trailer = `https://www.youtube.com/watch?v=${res.items[0].id}`;
-            }
-    
-            return {
-              tvDb: tvDbData,
-              trailer: trailer,
-            };
-          })
-          .catch((err) => {
-            console.log('Failed to fetch movie trailer', err)
-            throw new Error('Failed to fetch movie trailer');
-          });
-
-      }).catch((err) => {
-        console.log('TVDB search request failed', err);
-        throw new Error('TVDB search request failed');
-      });
+  async getSynopsisData(query) {
+    const tokenResponse = await this.login();
+    const movieDataResponse = await axios.get(`${URL_BASE}/search?query=${encodeURIComponent(query)}`, {
+      headers: `Authorization: Bearer ${tokenResponse.data.data.token}`
     }).catch((err) => {
-      console.log('failed to get TVDB token', err);
-      throw new Error('Failed to renew TVDB token');
+      console.log('TVDB search request failed', err);
+      throw new Error('TVDB search request failed');
     });
+
+    if (movieDataResponse.status !== 200) {
+      throw new Error('TVDB search request failed');
+    }
+    if (movieDataResponse.data.data.length === 0) {
+      throw new Error('TVDB search returned no results');
+    }
+    
+    const tvDbData = movieDataResponse.data.data[0];
+
+    const trailerData = await youtubesearchapi.GetListByKeyword(query + ' trailer', false, 1, {}).catch((err) => {
+      console.log('Failed to fetch movie trailer', err)
+      throw new Error('Failed to fetch movie trailer');
+    });
+
+    var trailer = '';
+    if (trailerData && trailerData.items && trailerData.items.length !== 0) {
+      trailer = `https://www.youtube.com/watch?v=${trailerData.items[0].id}`;
+    }
+
+    return {
+      tvDbData,
+      trailer,
+    };
   }
 }
